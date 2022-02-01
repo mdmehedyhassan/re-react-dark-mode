bf78917fb34f15dc6bbbc064cf5c7c9d7b9c94cd
unknown <mdmehedyh55@gamil.com>
2022-02-01 18:02:30 +0600

    link done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <GlobalStyles theme={theme ? lightTheme : darkTheme} />
       <h1>toggle theme</h1>
       <button onClick={() => setTheme(!theme)}> {theme ? 'dark' : 'light'}</button>
+      <a href="https://github.com/mdmehedyhassan/re-react-dark-mode">code link</a>
     </div>
   );
 }
